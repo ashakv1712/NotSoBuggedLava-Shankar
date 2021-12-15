ce3725fa7f11b3f40c82e0f33575a76dfe8f357e
ashakv1712 <74943973+ashakv1712@users.noreply.github.com>
2021-12-15 09:23:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas;
+var backgroundImage;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers 
+var player1Img, player1, player2Img, player2, boulderImg, boulder, fireballImg, fireball, player1Hurt, player2Hurt
+var wall
+var climber1, climber2;
+var climbers = [];
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.gif");
+  player1Img = loadAnimation("assets/player1Climb/Player1-1.png","assets/player1Climb/Player1-2.png","assets/player1Climb/Player1-3.png","assets/player1Climb/Player1-4.png","assets/player1Climb/Player1-5.png","assets/player1Climb/Player1-6.png")
+  player2Img = loadAnimation("assets/Player2Climb/Player2-1.png","assets/Player2Climb/Player2-2.png","assets/Player2Climb/Player2-3.png","assets/Player2Climb/Player2-4.png","assets/Player2Climb/Player2-5.png","assets/Player2Climb/Player2-6.png")
+  wall = loadImage("BackgroundForGame.png")
+}
+
+function setup() {
+  canvas = createCanvas(500, 600);
+  database = firebase.database();
+  game = new Game();
+  game.start();
+  game.getState();
+}
+
+function draw() {
+  
+  background(0);
+//  image(wall,0,-4000,380,-4000)
+  image(backgroundImage, 0,0,500,650)
+
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+   
+    game.end();
+  }
+}
